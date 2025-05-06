f31841268b98503480d8f09b6eb328fca8023a10
Satish Pandey <satishsrpandey@gmail.com>
2025-05-06 10:07:27 +0530

    Initial commit

---
+++ my-portfolio/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-portfolio';
+}
